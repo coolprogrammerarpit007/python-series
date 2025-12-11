84369674bb83c27b3a0f3f620131b6f0aeacbe8b
Arpit <arpit.mishra@microprixs.in>
2025-12-11 15:14:44 +0530

    completed the FIzzBuzz Challenge of day6

---
+++ FizzBuzz.py
@@ -0,0 +1,13 @@
+number = int(input("Enter Number: "))
+
+for num in range(1,number+1):
+    if num % 3 == 0 and num % 5 == 0:
+        print("FizzBuzz")
+    elif num % 3 == 0:
+        print("Fizz")
+        
+    elif num % 5 == 0:
+        print("Buzz")
+        
+    else:
+        print(num)
