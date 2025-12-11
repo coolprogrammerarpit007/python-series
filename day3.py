c09fe8b22327350b9587fd9d54b06a1b5159b309
Arpit <arpit.mishra@microprixs.in>
2025-12-11 12:19:06 +0530

    completed day3 + Exercises

---
+++ day3.py
@@ -0,0 +1,14 @@
+# greeting = "Hello, world!"
+# print(greeting)
+
+# person_name = input("Enter Name: ").strip().capitalize()
+# # print(f"Hello {person_name}")
+
+# print("I am {age}".format(age=29))
+
+title = "Joker"
+director = "Todd Phillips"
+release_year = 2019
+
+# print(f"Movie {title}, directed by: {director} and released in Year: {release_year}")
+print(f"{title} ({release_year}), directed by {director}")
